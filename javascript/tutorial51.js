9e812c965d2c186e8c15abb5e5ad4b2c8ec0a8a6
Ubaid Ur Rehman <ubaidurrehman.edu.pk@gmail.com>
2024-09-30 17:38:59 +0500

    this time lecture complete

---
+++ javascript/tutorial51.js
@@ -0,0 +1,38 @@
+console.log("this is tutorial 51");
+
+// console.log(name1 + " is a good boy")
+
+function greet(name1, greetText="greet text from javascript") {
+    console.log(greetText + " " + name1)
+    console.log(name1 + " is a good boy");
+}
+
+function sum(a,b,c){
+    let d = a + b + c;
+    return d;
+}
+
+let returnVal = sum(2, 3, 4)
+console.log(returnVal)
+
+
+
+// let name1 = "ubaid";
+// let name2 = "faisal";
+// let name3 = "mueid";
+// let name4 = "hafeez";
+// let name5 = "moosa";
+// let name6 = "esa";
+// let name7 = "yousuf";
+// let greetText = "good morning";
+
+// greet(name1, greetText);
+// greet(name2, greetText);
+// greet(name3, greetText);
+// greet(name4, greetText);
+// greet(name5, greetText);
+// greet(name6, greetText);
+// greet(name7);
+
+// let returnVal = greet(name1)
+// console.log(returnVal)
